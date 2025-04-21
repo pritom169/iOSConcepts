4302ef65383074e26f8496c873b5a60263bfc181
Pritom Mazumder <pritommazumder@gmail.com>
2025-04-21 23:34:23 +0200

    [UPDATE]: Moved test logic to a separate type "HTTPClientSpy"

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -9,23 +9,35 @@ import XCTest
 
 class RemoteFeedLoader {
     func load(){
-        HTTPClient.shared.requestedURL = URL(string: "https://example.com/feed.xml")
+        HTTPClient.shared.get(from: URL(string: "https://example.com/feed")!)
     }
 }
 
 class HTTPClient {
-    static let shared = HTTPClient()
+    static var shared = HTTPClient()
     
-    private init() {
-        
-    }
+    /// One of the common ways to make singleton testable is to
+    /// change the let to var.
+    ///
+    /// However it does not stays as singleton afterwards, as it turns
+    /// to sharable global state
     
+    func get(from url: URL) {}
+}
+
+class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+    
+    override func get(from url: URL){
+        requestedURL = url
+    }
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
+    
     func test_init_doesNotRequestDataFromURL(){
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         _ = RemoteFeedLoader()
         
         XCTAssertNil(client.requestedURL)
@@ -33,7 +45,8 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     func test_load_requestDataFromURL(){
         // Arrange
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         let sut = RemoteFeedLoader()
         
         // Act
